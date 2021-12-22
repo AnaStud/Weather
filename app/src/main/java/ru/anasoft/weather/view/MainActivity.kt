c80badcd36511aa06cca0fb9afd3f73d872b688f
Anna <anasoft@yandex.ru>
2021-12-22 21:49:43 +0500

    Добавила Binding

--- app/src/main/java/ru/anasoft/weather/view/MainActivity.kt
+++ app/src/main/java/ru/anasoft/weather/view/MainActivity.kt
@@ -3,11 +3,21 @@ package ru.anasoft.weather.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.anasoft.weather.R
+import ru.anasoft.weather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if(savedInstanceState == null){
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container,MainFragment.newInstance())
+                .commit()
+        }
     }
 }
