91283e92848b7374a03f980caf39d84f61c7b69b
Anna <anasoft@yandex.ru>
2022-01-22 09:55:41 +0500

    Homework 6. Доделала первое задание: Подпишитесь на событие изменения связи (CONNECTIVITY_ACTION) и уведомляйте об этом пользователя.

--- app/src/main/java/ru/anasoft/weather/view/MainActivity.kt
+++ app/src/main/java/ru/anasoft/weather/view/MainActivity.kt
@@ -1,7 +1,13 @@
 package ru.anasoft.weather.view
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ru.anasoft.weather.R
 import ru.anasoft.weather.databinding.ActivityMainBinding
 import ru.anasoft.weather.view.main.MainFragment
@@ -19,6 +25,21 @@ class MainActivity : AppCompatActivity() {
                 .beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commit()
+        }
+
+        val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+        registerReceiver(networkStateReceiver, filter)
+    }
+
+    private var networkStateReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.action == "android.net.conn.CONNECTIVITY_CHANGE") {
+                Toast
+                    .makeText(context,
+                        "Изменилось состояние подключения к Интернет",
+                        Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
 }
